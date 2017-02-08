23c39c8a526143a89b3ce5a52e1e892c6d84bebd
bavuong <bavuong@tais-MacBook-Pro-2.local>
2017-02-08 23:31:13 +0700

    git id when click button

--- js/components/footer/index.js
+++ js/components/footer/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Button from 'react-native-button';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 
-const getShitDone = (currentSwiperIndex, items) => {
-  const result = items[currentSwiperIndex];
-  return result!= null ? result.original_title: null;
+const getCurrentItem = (currentSwiperIndex, items) => items[currentSwiperIndex];
+
+const getAttribute = (currentSwiperIndex, items, key) => {
+  const currentItem = getCurrentItem(currentSwiperIndex, items);
+  return currentItem ? currentItem[key] : null;
 };
 
 const MyFooter = ({currentSwiperIndex, items}) => (
   <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-    <Text style={{ flex: 0.8}}>{getShitDone(currentSwiperIndex, items)}</Text>
+    <Text style={{ flex: 0.8}}>{getAttribute(currentSwiperIndex, items, 'original_title')}
+    </Text>
     <Button
       containerStyle={{flex: 0.2, height: 50, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center'}}
       style={{fontSize: 15, color: 'white', }}
-      onPress = {() => {console.log('I has been pressed!')}}
+      onPress = {() => {console.log(getAttribute(currentSwiperIndex, items, 'id'))}}
     >
       Press me!
     </Button>
